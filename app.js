a2e2ebb420690154de31345ee026efe3bc906f8d
Guillaume <delcourt.guillaume08@gmail.com>
2023-07-20 09:37:44 +0200

    Explication dun code

--- app.js
+++ app.js
@@ -7,64 +7,51 @@ let  tableauCity = ""
 
 
 
-function getValue() {
-    // Sélectionner l'élément input et récupérer sa valeur
-    let stateName = document.getElementById("selectState").value;
-    // Afficher la valeur
-    document.getElementById("selectState").innerHTML = `${stateName}`
+function getValue() { //fonction pour retourner un nom de région dans l'élément selectRegion
+    let stateName = document.getElementById("selectRegion").value;
+    document.getElementById("selectRegion").innerHTML = `${stateName}`
     return stateName
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdown = document.getElementById("selectState");
-    fetch ("http://api.airvisual.com/v2/states?country=France&key=2a1e45d1-95a2-46b6-b89c-46b687724e7a")
-    .then((response) => response.json())
-    .then((data) => {
+document.addEventListener("DOMContentLoaded", function () { //ça veut dire que la fonction s'exécute uniquement quand la page est complétement chargée
+    const dropdown = document.getElementById("selectRegion"); //dropdown = l'élément 'selectRegion'
+    fetch ("http://api.airvisual.com/v2/states?country=France&key=2a1e45d1-95a2-46b6-b89c-46b687724e7a") //requête à l'API airvisual qui va nous donner les données uniquement pour la France
+    .then((response) => response.json()) // première réponse de l'API (le then permet de ne pas marquer async)
+    .then((data) => {  // deuxième réponse de l'API qui nous renvoie les données relatives à la France
         // La réponse de l'API contient un objet "data" avec la liste des pays dans la propriété "data"
-        const regions = data.data;
-        regions.forEach((region) => {
+        let regions = data.data; // variable région
+        regions.forEach((region) => { // boucle qui dit que pour chaque région, ajouter l'élément dans le selectRegion
             const newOption = document.createElement("option");
-            newOption.text = region.state;
-            newOption.value = region.state; // Vous pouvez utiliser une autre propriété unique si nécessaire
-            dropdown.add(newOption);
+            newOption.text = region.state; // texte qui va être affiché dans l'option
+            newOption.value = region.state;  // valeur que va prendre l'option
+            dropdown.add(newOption); // commande pour ajouter l'élément dans le selectRegion
         });
         
     })
     .catch((error) => console.error("Erreur lors de la récupération des régions :", error));
 });
 
-document.getElementById("selectState").addEventListener("change",function (){
-    const stateName = document.getElementById("selectState").value;
-})
-
-
-document.getElementById("selectCity").addEventListener("change",function (){
-const cityName = document.getElementById("selectCity").value;
-
-})
-
-
-document.getElementById("selectState").addEventListener("change", function (){
-    (document.getElementById("selectCity")).innerHTML = '<option value="">--Sélectionnez votre ville --</option>';
-    const stateName = document.getElementById("selectState").value;
-    fetch (`http://api.airvisual.com/v2/cities?state=${stateName}&country=France&key=2a1e45d1-95a2-46b6-b89c-46b687724e7a`)
+document.getElementById("selectRegion").addEventListener("change", function (){ //fonction qui s'exécute uniquement quand il y a un changement dans 'selectRegion'
+    (document.getElementById("selectCity")).innerHTML = '<option value="">--Sélectionner votre ville --</option>'; // ajoute l'élement dans selectCity
+    const stateName = document.getElementById("selectRegion").value; //state name est = à la valeur rentrée dans le selectRegion
+    fetch (`http://api.airvisual.com/v2/cities?state=${stateName}&country=France&key=2a1e45d1-95a2-46b6-b89c-46b687724e7a`) // la valeur de la région choisi s'intègre dans l'url de l'API
         .then((response) => response.json())
         .then((data) => {
             // La réponse de l'API contient un objet "data" avec la liste des pays dans la propriété "data"
-            const cities = data.data;
-            cities.forEach((city) => {
+            const cities = data.data; // les villes sont sorties de l'API
+            cities.forEach((city) => { // pour chaque ville, cette boucle l'intègre au selectCity
                 const newOption = document.createElement("option");
                 newOption.text = city.city;
                 newOption.value = city.city; // Vous pouvez utiliser une autre propriété unique si nécessaire
-                (document.getElementById("selectCity")).appendChild(newOption);
+                (document.getElementById("selectCity")).add(newOption);
             });
         })
         .catch((error) => console.error("Erreur lors de la récupération des villes :", error));
 })
 
-async function callApiSearch(){
+async function callApiSearch(){ // fonction asynchrone qui va nous renvoyer les données de l'API pour la ville choisie
     const cityName = document.getElementById("selectCity").value
-    const stateName = document.getElementById("selectState").value
+    const stateName = document.getElementById("selectRegion").value
     const url = `http://api.airvisual.com/v2/city?city=${cityName}&state=${stateName}&country=France&key=${apiKey}`
     const fetcher = await fetch(url);
     const json = await fetcher.json();
@@ -73,20 +60,7 @@ async function callApiSearch(){
     console.log(tableauCity.location.coordinates)
     console.log(tableauCity.location.coordinates[0])
     console.log(tableauCity.location.coordinates[1]);
-    const map = new ol.Map({
-        layers: [
-          new ol.layer.Tile({
-            source: source
-          })
-        ],
-        controls: ol.control.defaults.defaults({attribution: false}).extend([attribution]),
-        target: 'map',
-        view: new ol.View({
-          constrainResolution: true,
-          center: ol.proj.fromLonLat([tableauCity.location.coordinates[0], tableauCity.location.coordinates[1]]),
-          zoom: 10
-        })
-      });
+
     }
 /*document.addEventListener("DOMContentLoaded", function () {
     const dropdown = document.getElementById("selectCity");
