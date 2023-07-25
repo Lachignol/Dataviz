5f76760f622305e33214ba1192d897015b60313c
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-07-25 10:08:57 +0200

    fixe:maj

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = "c8ca90b6-dd6a-48a4-b4d0-342b5c0e5eee"
+const apiKey = "caee4337-fbdc-4e36-9d58-696a7c41a1c9"
 
 
 
@@ -16,7 +16,7 @@ function getValue() { //fonction pour retourner un nom de région dans l'éléme
 
 document.addEventListener("DOMContentLoaded", function () { //ça veut dire que la fonction s'exécute uniquement quand la page est complétement chargée
     const dropdown = document.getElementById("selectRegion"); //dropdown = l'élément 'selectRegion'
-    fetch ("http://api.airvisual.com/v2/states?country=France&key=2a1e45d1-95a2-46b6-b89c-46b687724e7a") //requête à l'API airvisual qui va nous donner les données uniquement pour la France
+    fetch (`http://api.airvisual.com/v2/states?country=France&key=${apiKey}`) //requête à l'API airvisual qui va nous donner les données uniquement pour la France
     .then((response) => response.json()) // première réponse de l'API (le then permet de ne pas marquer async)
     .then((data) => {  // deuxième réponse de l'API qui nous renvoie les données relatives à la France
         // La réponse de l'API contient un objet "data" avec la liste des pays dans la propriété "data"
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () { //ça veut dire que 
 document.getElementById("selectRegion").addEventListener("change", function (){ //fonction qui s'exécute uniquement quand il y a un changement dans 'selectRegion'
     (document.getElementById("selectCity")).innerHTML = '<option value="">--Sélectionner votre ville --</option>'; // ajoute l'élement dans selectCity
     const stateName = document.getElementById("selectRegion").value; //state name est = à la valeur rentrée dans le selectRegion
-    fetch (`http://api.airvisual.com/v2/cities?state=${stateName}&country=France&key=2a1e45d1-95a2-46b6-b89c-46b687724e7a`) // la valeur de la région choisi s'intègre dans l'url de l'API
+    fetch (`http://api.airvisual.com/v2/cities?state=${stateName}&country=France&key=${apiKey}`) // la valeur de la région choisi s'intègre dans l'url de l'API
         .then((response) => response.json())
         .then((data) => {
             // La réponse de l'API contient un objet "data" avec la liste des pays dans la propriété "data"
@@ -57,14 +57,19 @@ async function callApiSearch(){ // fonction asynchrone qui va nous renvoyer les 
     const fetcher = await fetch(url);
     const json = await fetcher.json();
     tableauCity = json.data
-    console.table(tableauCity)
-    console.log(tableauCity.location.coordinates)
-    console.log(tableauCity.location.coordinates[0])
-    console.log(tableauCity.location.coordinates[1]);
-
+    let qualitéAir = tableauCity.current.pollution.aqicn
+    let indiceDePollution = tableauCity.current.pollution.aqius
+    let temperature = tableauCity.current.weather.tp
+    console.log(tableauCity)
     map.getView().setCenter(ol.proj.fromLonLat([tableauCity.location.coordinates[0],tableauCity.location.coordinates[1]]))
     map.getView().setZoom(10)   
+    document.getElementById("affichageResult").innerHTML = `<ul><li>qualité de l'air : ${qualitéAir}</li> <li>indice de pollution :${indiceDePollution}</li> <li>temperature : ${temperature} °c</li></ul> `
+  
+
+
 }
+
+
 /*document.addEventListener("DOMContentLoaded", function () {
     const dropdown = document.getElementById("selectCity");
     fetch (`http://api.airvisual.com/v2/cities?state=${stateName}&country=France&key=2a1e45d1-95a2-46b6-b89c-46b687724e7a`)
@@ -108,8 +113,16 @@ async function callApiIp(){
     const url = `http://api.airvisual.com/v2/nearest_city?key=${apiKey}`
     const fetcher = await fetch(url);
     const json = await fetcher.json();
-    console.log(json);
-    console.log(json.data.location.coordinates)
+    tableauCity = json.data
+    let countryName = tableauCity.country
+    let qualitéAir = tableauCity.current.pollution.aqicn
+    let indiceDePollution = tableauCity.current.pollution.aqius
+    let temperature = tableauCity.current.weather.tp
+    console.log(tableauCity)
+    map.getView().setCenter(ol.proj.fromLonLat([tableauCity.location.coordinates[0],tableauCity.location.coordinates[1]]))
+    map.getView().setZoom(10)   
+    document.getElementById("affichageResult").innerHTML = `<ul><li> Pays : ${countryName}</li> <li>qualité de l'air : ${qualitéAir}</li> <li>indice de pollution :${indiceDePollution}</li> <li>temperature : ${temperature} °c</li></ul> `
+  
 
 }
 
